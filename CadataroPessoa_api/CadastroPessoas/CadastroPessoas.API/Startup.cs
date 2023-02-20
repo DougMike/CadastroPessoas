6e6da814dd6d52e81f20c67ed118a4f43b476d59
Mike D. Saint <douglasmikemd@gmail.com>
2023-02-20 13:05:18 -0300

    .

--- CadataroPessoa_api/CadastroPessoas/CadastroPessoas.API/Startup.cs
+++ CadataroPessoa_api/CadastroPessoas/CadastroPessoas.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 namespace CadastroPessoas.API
 {
@@ -40,6 +41,10 @@ namespace CadastroPessoas.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CadastroPessoas.API", Version = "v1" });
             });
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
